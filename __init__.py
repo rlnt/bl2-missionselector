21e98082c14fd42e156ff3abad217088091843d9
Chronophylos <accounts@chronophylos.com>
2021-03-18 23:16:03 +0100

    use eridium

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
-import unrealsdk
-import enum
 import webbrowser
-from typing import Dict, List, Optional
+from typing import Dict, Iterable
 from Mods.ModMenu import (
     SDKMod,
     Mods,
@@ -9,10 +7,11 @@ from Mods.ModMenu import (
     EnabledSaveType,
     KeybindManager,
     Keybind,
-    ServerMethod,
     RegisterMod,
 )
-from Mods.Eridium import KeyBinds, log
+from Mods.Eridium import log
+from Mods.Eridium.keys import KeyBinds
+from Mods.Eridium.missions import MissionTracker, Mission
 
 NEXT_MISSION_DESC: str = "Select next Mission"
 NEXT_MISSION_KEY: str = KeyBinds.RightBracket
@@ -20,20 +19,14 @@ PREV_MISSION_DESC: str = "Select previous Mission"
 PREV_MISSION_KEY: str = KeyBinds.LeftBracket
 
 
-class EMissionStatus(enum.IntEnum):
-    NotStarted = 0
-    Active = 1
-    RequiredObjectivesComplete = 2
-    ReadyToTurnIn = 3
-    Complete = 4
-    Failed = 5
-    MAX = 6
-
-    def isActive(self) -> bool:
-        return self in [
-            EMissionStatus.ReadyToTurnIn,
-            EMissionStatus.Active,
-        ]
+def getActiveMissionIndex(
+    missionTracker: MissionTracker, missions: Iterable[Mission]
+) -> int:
+    activeMission = missionTracker.ActiveMission
+    for index, mission in enumerate(missions):
+        if mission.MissionDef.MissionNumber == activeMission.MissionNumber:
+            return index
+    return -1
 
 
 class MissionSelector(SDKMod):
@@ -45,7 +38,7 @@ class MissionSelector(SDKMod):
     Types: ModTypes = ModTypes.Utility
     SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
 
-    SettingsInputs: Dict[str, str] = {"Enter": "Enable", "G": "Github"}
+    SettingsInputs: Dict[str, str] = {KeyBinds.Enter: "Enable", KeyBinds.G: "Github"}
 
     def __init__(self) -> None:
         super().__init__()
@@ -78,85 +71,26 @@ class MissionSelector(SDKMod):
             webbrowser.open("https://github.com/Chronophylos/bl2_missionselector")
 
     def NextMission(self) -> None:
-        missions = self.GetActiveMissions()
-        active_mission_index = self._getActiveMissionIndex(missions)
+        missionTracker = MissionTracker()
+        activeMissions = missionTracker.getActiveMissions()
+        index = getActiveMissionIndex(missionTracker, activeMissions)
 
-        next_mission = None
-        if active_mission_index < len(missions) - 1:
-            next_mission = missions[active_mission_index + 1]
+        nextMission: Mission = None
+        if index < len(activeMissions) - 1:
+            nextMission = activeMissions[index + 1]
         else:
-            next_mission = missions[0]
+            nextMission = activeMissions[0]
 
-        self.SetSelectedMission(next_mission.missionDef)
+        missionTracker.setActiveMission(nextMission.MissionDef)
 
     def PrevMission(self) -> None:
-        missions = self.GetActiveMissions()
-        active_mission_index = self._getActiveMissionIndex(missions)
+        missionTracker = MissionTracker()
+        activeMissions = missionTracker.getActiveMissions()
+        index = getActiveMissionIndex(missionTracker, activeMissions)
 
-        next_mission = missions[active_mission_index - 1]
+        nextMission = activeMissions[index - 1]
 
-        self.SetSelectedMission(next_mission.missionDef)
-
-    def _isClient(self) -> bool:
-        return int(unrealsdk.GetEngine().GetCurrentWorldInfo().NetMode) == 3
-
-    def _getMissionTracker(self) -> unrealsdk.UObject:
-        """Return the `WillowGame.MissionTracker`."""
-        return unrealsdk.GetEngine().GetCurrentWorldInfo().GRI.MissionTracker
-
-    def _getActiveMissionIndex(self, missions: List[unrealsdk.UObject]) -> int:
-        active_mission = self.GetSelectedMission()
-        for i, m in enumerate(missions):
-            if m.MissionDef.MissionNumber == active_mission.MissionNumber:
-                return i
-        return -1
-
-    def GetActiveMissions(self) -> List[unrealsdk.UObject]:
-        """
-        Get all missions that are either active or ready to turn in.
-        Returns list of `IMission.MissionData`
-        """
-        missions = []
-        for mission in self._getMissionTracker().MissionList:
-            if EMissionStatus(mission.Status).isActive():
-                missions.append(mission)
-        return sorted(missions, key=lambda x: int(x.MissionDef.MissionNumber))
-
-    def GetSelectedMission(self) -> unrealsdk.UObject:
-        """Return the selected mission as `WillowGame.MissionDefinition`."""
-        return self._getMissionTracker().GetActiveMission()
-
-    def GetMissionByNumber(self, number: int) -> Optional[unrealsdk.UObject]:
-        for mission in self._getMissionTracker().MissionList:
-            if mission.MissionDef.MissionNumber == number:
-                return mission.MissionDef
-        return None
-
-    def SetSelectedMission(self, mission: unrealsdk.UObject) -> None:
-        """Sets the selected mission.
-
-        mission must be a `WillowGame.MissionDefinition`.
-        """
-
-        if self._isClient():
-            self._serverSetSelectedMission(mission.MissionNumber)
-        else:
-            self._setSelectedMission(mission.MissionNumber)
-
-    @ServerMethod
-    def _serverSetSelectedMission(
-        self, number: int, PC: Optional[unrealsdk.UObject] = None
-    ) -> None:
-        self._setSelectedMission(number, PC)
-
-    def _setSelectedMission(
-        self, number: int, PC: Optional[unrealsdk.UObject] = None
-    ) -> None:
-        mission = self.GetMissionByNumber(number)
-        if not mission:
-            log(self, "Could not find mission with number {number}")
-            return
-        self._getMissionTracker().SetActiveMission(mission, True, PC)
+        missionTracker.setActiveMission(nextMission.MissionDef)
 
 
 instance = MissionSelector()
